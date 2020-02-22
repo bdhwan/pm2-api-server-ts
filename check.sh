ba7b045b2385ac3da7b6d2f7b6be243d909168ee
bdhwan <bdhwan@gmail.com>
2020-02-22 22:24:09 +0900

    change run script to source procject

--- check.sh
+++ check.sh
@@ -4,16 +4,19 @@ echo $GIT_URL
 
 # git clone $GIT_URL
 git clone --depth 1 --single-branch --branch master "$GIT_URL" && cd "$(basename "$GIT_URL" .git)"
+sh start.sh
 
-if [ -z "$PM2_INSTANCE" ]
-then
-    echo "no instance count set"
-else 
-    echo "have instance count set-"$PM2_INSTANCE
-    # sed -i -e "s/instances: 1/instances: "$PM2_INSTANCE"/g" process.yml 
-    sed -i -- 's#instances: 1#instances: '"$PM2_INSTANCE"'#g' process.yml 
-fi
 
-npm install --production
-tsc
-pm2-docker process.yml
+
+# if [ -z "$PM2_INSTANCE" ]
+# then
+#     echo "no instance count set"
+# else 
+#     echo "have instance count set-"$PM2_INSTANCE
+#     # sed -i -e "s/instances: 1/instances: "$PM2_INSTANCE"/g" process.yml 
+#     sed -i -- 's#instances: 1#instances: '"$PM2_INSTANCE"'#g' process.yml 
+# fi
+
+# npm install --production
+# tsc
+# pm2-docker process.yml
